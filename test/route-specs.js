8cc9b5a4607502796635dba9b32806d6ce07ec32
Andrew Heller <andrewhheller@gmail.com>
2018-11-28 20:32:53 -0500

    created basic supertest tests

--- test/route-specs.js
+++ test/route-specs.js
@@ -4,29 +4,30 @@ const app = supertest(require('../server/app'));
 const expect = require('chai').expect;
 
 
-describe('Escape ARoom main game route', () => {
+describe('main game routes', () => {
 
   it('can reach the home page', () => {
     return app
       .get('/')
       .expect(200)
+  })
+
+  it('displays home page content', () => {
+    return app
+      .get('/')
       .expect(response => {
           expect(response.text).to.contain('Escape')
           expect(response.text).not.to.contain('Grace Shopper')
       })
   })
 
-})
-
-
-describe('Error Handling', () => {
-
   it('displays a 404 error page', () => {
     return app
       .get('/grace-shopper')
       .expect(404)
       .expect(response => {
-        expect(response.text).to.contain('')
+        expect(response.text).to.contain('Page Not Found') // browser tab title text
+        expect(response.text).not.to.contain('Escape')
       })
   })
 
