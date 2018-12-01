b018bd8c6867018feefe7c1f3a6ad0a598b723d3
Andrew Heller <andrewhheller@gmail.com>
2018-12-01 13:36:11 -0500

    tested ENV file ok

--- server/app.js
+++ server/app.js
@@ -3,13 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const jwt = require('jwt-simple');
-// const env = require('dotenv');
+const env = require('dotenv');
 
 // instantiate express
 const app = express();
 
 // dotenv file config
-// env.config();
+env.config();
 
 // data models
 const { User } = require('./db').models;
