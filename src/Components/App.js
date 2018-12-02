8d8fc8f25fb25a959f3b9e4c80abe80c543ce6c1
Andrew Reis <areis89@gmail.com>
2018-12-02 18:27:35 -0500

    connected

--- src/Components/App.js
+++ src/Components/App.js
@@ -21,56 +21,61 @@ import Login from './Login';
 import Account from './Account';
 import Signup from './Signup';
 
-
 class App extends Component {
-
   componentDidMount() {
     this.props.init();
   }
 
   render() {
-    const { auth } = this.props
+    const { auth } = this.props;
     if (!auth.id) {
       return (
         <Router>
-          <div className='background'>
+          <div className="background">
             <Switch>
-              <Route path="/signup" render={(props) => <Signup props={props} /> } />
-              <Route path="/" render={ (props) => <Login props={props} /> } />
+              <Route
+                path="/signup"
+                render={props => <Signup props={props} />}
+              />
+              <Route path="/" render={props => <Login props={props} />} />
             </Switch>
           </div>
         </Router>
-      )
+      );
     }
-    return ( //add account Component, finish Success page
+    return (
+      //add account Component, finish Success page
       <Router>
         <Fragment>
-          <Route exact path="/" component={ Enter } />
-          <Route path="/account" component={ Account } />
-          <Route path="/info" component={ GamePlay } />
-          <Route path="/signup" render={(props) => <Signup props={props} /> } />
-          <Route path="/temp" component={ Temp } />
-          <Route path="/room" component={ Room } />
-          <Route exact path="/room/clock" component={ Clock } />
-          <Route exact path="/room/lock" component={ Lock } />
-          <Route exact path="/room/envelope" component={ Envelope } />
-          <Route exact path="/room/letter" component={ Letter } />
-          <Route exact path="/escaped" component={ Escaped } />
-          <Route exact path="/opening" component={ Opening } />
+          <Route exact path="/" component={Enter} />
+          <Route path="/account" component={Account} />
+          <Route path="/info" component={GamePlay} />
+          <Route path="/signup" render={props => <Signup props={props} />} />
+          <Route path="/temp" component={Temp} />
+          <Route path="/room" component={Room} />
+          <Route exact path="/room/clock" component={Clock} />
+          <Route exact path="/room/lock" component={Lock} />
+          <Route exact path="/room/envelope" component={Envelope} />
+          <Route exact path="/room/letter" component={Letter} />
+          <Route exact path="/escaped" component={Escaped} />
+          <Route exact path="/opening" component={Opening} />
         </Fragment>
       </Router>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  auth: state.auth
+const mapStateToProps = state => ({
+  auth: state.auth,
 });
 
 const mapDispatchToProps = dispatch => ({
   init: () => {
-    dispatch(exchangeTokenForAuth())
-  }
-})
+    dispatch(exchangeTokenForAuth());
+  },
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
