024c97fa60b150878499a868c9fdeec1286dfefa
Charlie <cr646531@gmail.com>
2018-12-04 14:32:25 -0500

    added lockpicking to the testing suite

--- src/Components/Testing.js
+++ src/Components/Testing.js
@@ -5,6 +5,7 @@ import Clock from './Clock';
 import Letter from './Letter';
 import Sound from 'react-sound';
 import Escaped from './Escaped';
+import Inventory from './Inventory';
 
 
 export default class Testing extends React.Component {
@@ -12,39 +13,60 @@ export default class Testing extends React.Component {
     super();
     this.state = {
       key: false,
+      letter: false,
+      pick: false,
       open: false,
       showClock: false,
       showLock: false,
       showLetter: false,
       showDoor: false,
       knock: false,
-      bang: false
+      bang: false,
+      squeak: false,
+      creak: false,
+      snap: false,
+      flag: false
     };
     this.stopKnock = this.stopKnock.bind(this);
     this.stopBang = this.stopBang.bind(this);
+    this.stopSqueak = this.stopSqueak.bind(this);
+    this.stopCreak = this.stopCreak.bind(this);
+    this.stopSnap = this.stopSnap.bind(this);
     this.receiveKey = this.receiveKey.bind(this);
+    this.checkBehind = this.checkBehind.bind(this);
+    this.pickLock = this.pickLock.bind(this);
   }
 
   stopKnock(){ this.setState({ knock: false }) };
   stopBang(){ this.setState({ bang: false }) };
+  stopSqueak(){ this.setState({ squeak: false }) };
+  stopCreak(){ this.setState({ creak: false }) };
+  stopSnap(){ this.setState({ snap: false }) };
   receiveKey(){ this.setState({ key: true }) };
+  checkBehind(){ alert('You found an old lockpick'); this.setState({ pick: true, creak: true }); };
+  pickLock(){ alert('It broke!'); this.setState({ pick: false, snap: true }); };
 
   render(){
 
-    const { key, open, showClock, showLock, showLetter, showDoor, knock, bang } = this.state;
+    const { key, letter, pick, open, showClock, showLock, showLetter, showDoor, knock, bang, squeak, creak, flag, snap } = this.state;
 
     return (
-
       <div className="button-grid-container">
         <div className="button-grid-item">
-          <button className="welcome-btn" onClick={()=> this.setState({ showClock: true})}>Clock</button>
-          <button className="welcome-btn" onClick={()=> this.setState({ showLock: true})}>Lock</button>
-          <button className="welcome-btn" onClick={()=> this.setState({ showLetter: true})}>Letter</button>
-          <button className="welcome-btn" onClick={()=> this.setState({ showDoor: true})}>Door</button>
+
+          {/* ----- Buttons ----- */}
+
+          <button className="welcome-btn" onClick={()=> this.setState({ showClock: true })}>Clock</button>
+          <button className="welcome-btn" onClick={()=> this.setState({ showLock: true })}>Lock</button>
+          <button className="welcome-btn" onClick={()=> this.setState({ showLetter: true, letter: true })}>Letter</button>
+          <button className="welcome-btn" onClick={()=> this.setState({ showDoor: true })}>Door</button>
+
+          {/* ----- Components ----- */}
 
           {showClock && (
             <div>
               <Clock />
+              <button className="welcome-btn" onClick={this.checkBehind}>Check Behind Clock</button>
               <button onClick={()=> this.setState({ showClock: false }) } >x</button>
             </div>
           )}
@@ -66,34 +88,31 @@ export default class Testing extends React.Component {
           {showDoor && (
             <div>
               <br />
-              <button className="welcome-btn" onClick={()=> this.setState({ knock: true }) }>Knock</button>
-              <button className="welcome-btn" onClick={()=> this.setState({ bang: true }) }>Bang</button>
-              {key && (
-                <button className="welcome-btn" onClick={()=> this.setState({ open: true }) }>Open</button>
-              )}
+              { !flag && <button className="welcome-btn" onClick={()=> this.setState({ knock: true, flag: true }) }>Knock</button> }
+              { flag && <button className="welcome-btn" onClick={()=> this.setState({ bang: true }) }>Bang</button> }
+              { pick && <button className="welcome-btn" onClick={ this.pickLock }>Pick</button> }
+              { key && <button className="welcome-btn" onClick={()=> this.setState({ open: true }) }>Open</button> }
               <button onClick={()=> this.setState({ showDoor: false }) } >x</button>
               <br />
             </div>
           )}
 
-          {knock && ( 
-            <div>
-              <Sound url={'knocking.mp3'} playStatus={Sound.status.PLAYING} onFinishedPlaying={this.stopKnock} />
-            </div>
-          )}
+          {/* ----- Sounds ----- */}
 
-          {bang && (
-            <div>
-              <Sound url={'banging.mp3'} playStatus={Sound.status.PLAYING} onFinishedPlaying={this.stopBang} />
-            </div>
-          )}
+          { knock && <Sound url={'knocking.mp3'} playStatus={Sound.status.PLAYING} onFinishedPlaying={this.stopKnock} /> }
+          { bang && <Sound url={'banging.mp3'} playStatus={Sound.status.PLAYING} onFinishedPlaying={this.stopBang} /> }
+          { squeak && <Sound url={'door.mp3'} playStatus={Sound.status.PLAYING} onFinishedPlaying={this.stopSqueak} /> }
+          { creak && <Sound url={'creaking.mp3'} playStatus={Sound.status.PLAYING} onFinishedPlaying={this.stopCreak} /> }
+          { snap && <Sound url={'snapping.mp3'} playStatus={Sound.status.PLAYING} onFinishedPlaying={this.stopSnap} /> }
 
-          {open && (
-            <div>
-              <Escaped />
-              <Sound url={'door.mp3'} playStatus={Sound.status.PLAYING} onFinishedPlaying={this.stopBang} />
-            </div>
-          )}
+          {/* ----- Finish ----- */}
+
+          {open && <Escaped /> }
+
+
+          {/* ----- Inventory ----- */}
+
+          <Inventory hasKey={key} hasLetter={letter} hasLockPick={pick} />
 
         </div>
       </div>
@@ -101,117 +120,3 @@ export default class Testing extends React.Component {
     )
   }
 }
-
-
-
-
-
-
-          /* {clock && (
-            <div>
-              <button className="welcome-btn" onClick={() => this.setState({ showClock: true })}>
-                Check the time
-              </button>
-              <button className="welcome-btn" onClick={this.setState({ playCreakingSound: true })}>
-                {this.state.playCreakingSound && (
-                  <Sound
-                    url={'creaking.mp3'}
-                    playStatus={Sound.status.PLAYING}
-                    onFinishedPlaying={this.stopPlayingCreakingSound}
-                  />
-                )}
-                Look behind clock
-              </button>
-            </div>
-          )}
-          {showClock && <Clock />}
-          {letter && (
-              <button
-                className="welcome-btn"
-                onClick={() => this.setState({ hasLetter: true, displayLetter: true })}
-              >
-                Read me
-              </button>
-          )}
-          {lock && (
-            <button
-              className="welcome-btn"
-              onClick={() => {
-                this.setState({ showLock: true })
-              }}
-            >
-              Unlock Me
-            </button>
-          )}
-          {displayLetter && (
-            <div>
-              <Letter />
-              <button onClick={()=> this.setState({ displayLetter: false }) }>
-                x
-              </button>
-            </div>
-          )}
-          {showLock && (
-            <div>
-              <Lock receiveKey={this.state.receiveKey}/>
-            </div>
-          )}
-          {door && (
-            <div>
-              {hasKey ? (
-                <Link to="/escaped">
-                  <button className="welcome-btn" onClick={this.removeCamera}>
-                    Open Door
-                  </button>
-                </Link>
-              ) : (
-                <button
-                  className="welcome-btn"
-                  onClick={() => alert('You need a key')}
-                >
-                  Open Door
-                </button>
-              )}
-
-              <button className="welcome-btn" onClick={this.bangDoor}>
-              {this.state.playBangingSound && (
-                  <Sound
-                    url={'banging.mp3'}
-                    playStatus={Sound.status.PLAYING}
-                    onFinishedPlaying={this.stopPlayingBangingSound}
-                  />
-                )}
-                {this.state.playKnockingSound && (
-                  <Sound
-                    url={'knocking.mp3'}
-                    playStatus={Sound.status.PLAYING}
-                    onFinishedPlaying={this.stopPlayingKnockingSound}
-                  />
-                )}
-                {hasNote ? 'Bang louder' : 'Bang against the door'}
-              </button>
-            </div>
-          )}
-
-          {door && hasLockPick && (
-            <button className="welcome-btn" onClick={this.useLockPick}>
-              Use Lock Pick
-            </button>
-          )}
-        </div>
-        <Stopwatch />
-        <Inventory
-          hasKey={hasKey}
-          hasLetter={hasLetter}
-          hasNote={hasNote}
-          hasLockPick={hasLockPick}
-          hasBrokenLockPick={hasBrokenLockPick}
-        />
-        <Link to="/testing">Testing</Link>
-        <Link to="/escaped">
-            <button className="welcome-btn" onClick={this.removeCamera}>
-              Open Door
-            </button>
-        </Link>
-      </div> */
-
